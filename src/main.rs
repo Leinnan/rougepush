ae54f2371e8f736222501dd6afa109bbcd757ae1
Piotr Siuszko <siuszko@zoho.com>
2024-03-01 23:30:10 +0100

    Init

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@
 // workflow treats them as errors, so this allows them throughout the project.
 // Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
-
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use bevy::prelude::*;
 
 fn main() {
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_systems(Startup, setup)
         .run();
 }
