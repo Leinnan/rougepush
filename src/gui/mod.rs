1ffd5ff7145b02060de9f99777d482a9ef9a32d6
Piotr Siuszko <siuszko@zoho.com>
2024-03-07 22:03:30 +0100

    Keyboard GUI improvements

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -3,6 +3,8 @@ use bevy::prelude::*;
 use crate::{
     board::components::*,
     consts::{BASE_FONT, MY_ACCENT_COLOR},
+    despawn_recursive_by_component,
+    input::InputAction,
     states::*,
 };
 
@@ -11,7 +13,16 @@ use self::death_screen::DeathScreenButton;
 mod death_screen;
 
 #[derive(Component, Reflect)]
+pub struct CurrentActorInfoRoot;
+
+#[derive(Component, Reflect)]
 pub struct CurrentActorInfo;
+
+#[derive(Component, Reflect)]
+pub struct ActionInfo {
+    pub action: InputAction,
+    pub description: String,
+}
 
 pub struct GameGuiPlugin;
 
@@ -19,16 +30,22 @@ impl Plugin for GameGuiPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<CurrentActorInfo>()
             .register_type::<DeathScreenButton>()
+            .register_type::<ActionInfo>()
             .add_systems(OnEnter(MainGameState::Game), add_actor_info)
             .add_systems(
                 OnEnter(GameTurnSteps::ActionSelection),
                 update_info.after(PreparingActions::TrimWrongMoves),
             )
             .add_systems(
+                OnExit(GameTurnSteps::ActionSelection),
+                despawn_recursive_by_component::<ActionInfo>,
+            )
+            .add_systems(
                 Update,
                 (
                     death_screen::handle_death_menu_buttons,
                     death_screen::create_death_screen,
+                    spawn_action_info,
                 ),
             );
     }
@@ -36,46 +53,97 @@ impl Plugin for GameGuiPlugin {
 
 fn add_actor_info(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
-        .spawn(
-            TextBundle::from_section(
-                "".to_string(),
-                TextStyle {
-                    font: asset_server.load(BASE_FONT),
-                    font_size: 20.0,
-                    color: MY_ACCENT_COLOR,
-                },
-            )
-            .with_text_justify(JustifyText::Left)
-            .with_style(Style {
+        .spawn(NodeBundle {
+            style: Style {
                 position_type: PositionType::Absolute,
+                flex_direction: FlexDirection::Column,
                 top: Val::Px(5.0),
                 left: Val::Px(5.0),
                 ..default()
-            }),
-        )
-        .insert(CurrentActorInfo)
-        .insert(GameObject);
+            },
+            ..default()
+        })
+        .insert(GameObject)
+        .insert(CurrentActorInfoRoot)
+        .with_children(|root| {
+            root.spawn(
+                TextBundle::from_section(
+                    "".to_string(),
+                    TextStyle {
+                        font: asset_server.load(BASE_FONT),
+                        font_size: 20.0,
+                        color: MY_ACCENT_COLOR,
+                    },
+                )
+                .with_text_justify(JustifyText::Left),
+            )
+            .insert(CurrentActorInfo);
+        });
 }
 
 fn update_info(
-    mut q: Query<&mut Text, With<CurrentActorInfo>>,
+    mut commands: Commands,
+    mut q: Query<(&mut Text, &Parent), With<CurrentActorInfo>>,
     q2: Query<(&PossibleActions, Option<&PlayerControl>, &Piece), With<CurrentActorToken>>,
 ) {
-    let Ok(mut t) = q.get_single_mut() else {
+    let Ok((mut t, parent)) = q.get_single_mut() else {
         return;
     };
     let Ok((possible_actions, player_control, piece)) = q2.get_single() else {
         return;
     };
-    let mut value = format!("{:?} turn\n", piece);
+
     if player_control.is_some() {
-        for action in possible_actions.0.iter() {
-            value.push_str(&format!(
-                "{:?}-> {:?}\n",
-                action.get_input().unwrap(),
-                action.action_type()
-            ));
-        }
+        commands.entity(**parent).with_children(|p| {
+            for a in possible_actions.0.iter() {
+                let Some(action) = a.get_input() else {
+                    continue;
+                };
+                p.spawn(ActionInfo {
+                    action,
+                    description: format!("{:?}", a.action_type()),
+                })
+                .insert(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Row,
+                        padding: UiRect::all(Val::Px(5.0)),
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    ..default()
+                });
+            }
+        });
     }
-    t.sections[0].value = value;
+    t.sections[0].value = format!("{:?} turn\n", piece);
+}
+
+fn spawn_action_info(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    q: Query<(Entity, &ActionInfo), Added<ActionInfo>>,
+) {
+    for (e, info) in q.iter() {
+        commands.entity(e).with_children(|r| {
+            let img = match info.action {
+                InputAction::Left => "ui/keyboard_arrows_left_outline.png".to_owned(),
+                InputAction::Right => "ui/keyboard_arrows_right_outline.png".to_owned(),
+                InputAction::Up => "ui/keyboard_arrows_up_outline.png".to_owned(),
+                InputAction::Down => "ui/keyboard_arrows_down_outline.png".to_owned(),
+            };
+            r.spawn(ImageBundle {
+                image: UiImage::new(asset_server.load(img)),
+                background_color: MY_ACCENT_COLOR.into(),
+                ..default()
+            });
+            r.spawn(TextBundle::from_section(
+                info.description.clone(),
+                TextStyle {
+                    font: asset_server.load(BASE_FONT),
+                    font_size: 15.0,
+                    color: MY_ACCENT_COLOR,
+                },
+            ));
+        });
+    }
 }
