da439e81a2b5f8596a370eca5ea2ae60356c1941
Leinnan <siuszko@zoho.com>
2025-02-19 20:01:01 +0000

    Deploying to gh-pages from @ Leinnan/rougepush@4399f6b7b5974b5918d551d2d450a2520740ffba 🚀

---
+++ rougepush_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc20e1eb355647dc4: (a: number, b: number, c: number) => void;
+export const closure22009_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h03c2ffd1407d3ba6: (a: number, b: number) => void;
+export const closure22016_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h36730bb286b010ac: (a: number, b: number) => void;
+export const closure129482_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
